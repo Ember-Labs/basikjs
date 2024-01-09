ad0f3d74a12d97e5dea1b5ae2be4600322dae58f
Scarlet Rose <70824102+scarletquasar@users.noreply.github.com>
2024-01-08 22:57:58 -0300

    Initial commit

---
+++ BasikJS/Program.cs
@@ -0,0 +1,13 @@
+﻿using BasikJS.Entities;
+
+namespace BasikJS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var cli = new BasikCLI();
+            cli.WaitForNext();
+        }
+    }
+}
